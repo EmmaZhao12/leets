c6cc87ea0366a4892c068ab82d5c228f5978bd11
Emma Zhao <emmazhao05@gmail.com>
2025-04-29 16:37:09 -0400

    bruh it works but not in leetcode

--- 88.java
+++ 88.java
@@ -1,26 +1,35 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums3 = new int[nums1.length];
-        int i = 0;
-        int j = 0;
-        int curr = 0;
-        while(i < m && j < n){
-            var e1 = nums1[i];
-            var e2 = nums2[j];
-            if (e1 < e2){
-                nums3[curr] = e1;
-                System.out.print(e1);
-                i++;
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+            int[] nums3 = new int[nums1.length];
+            int i = 0;
+            int j = 0;
+            int curr = 0;
+            if (n == 0 || m == 0){
+                if(m==0){
+                    nums1 = nums2;
+                }
+                return;
             }
-            else{
-                nums3[curr] = e2;
-                System.out.print(e2);
-                j++;
+            while(curr < nums1.length){
+                var e1 = nums1[i];
+                var e2 = nums2[j];
+                if (e1 < e2 && e1 != 0){
+                    nums3[curr] = e1;
+                    i++;
+                }
+                else{
+                    nums3[curr] = e2;
+                    j++;
+                }
+                curr++;
             }
-            curr++;
+            nums1 = nums3;
         }
-    }
-    public static void main(String[] args){
-        merge([1,2,3,0,0,0], 3, [2,5,6], 3);
+        public static void main(String[] args){
+            int[] a = {1, 2, 3, 0, 0, 0};
+            int[] b = {2, 5, 6};
+            merge(a, 3, b, 3);
     }
 }
+
+
